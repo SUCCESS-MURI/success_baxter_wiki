5925ca344a48cca93436c016786adcaf04ba4759
jmc359 <jconnolly171@gmail.com>
2019-08-16 13:44:22 -0400

    update kinect transform code

--- correct_kinect_transform.py
+++ correct_kinect_transform.py
@@ -1,42 +1,58 @@
-'''
-@author: Sarthak Ahuja
+#!/usr/bin/env python
 
-Sample Run:
-python correct_kinect_transform.py --trans 0.044 0.080 0.619 --euler -0.067 1.103 -0.045
-
-Expected Output:
-There you go -
-rosrun tf static_transform_publisher 0.044 0.08 0.619 1.63779632679 3.18659265359 0.467796326795 /reference/base /kinect2_link 10
-'''
-
+import os
+import tf
+import rospy
 import argparse
 import numpy as np
 
-def main(trans, euler):
+"""
+Node for automatically generating commands for publishing the static transform 
+between base and kinect frames during calibration
+"""
+
+def transform_command(base_frame, kinect_frame, trans, euler):
     '''
-    The baxter_kinect_calibration package returns the following when you run "rosrun tf tf_echo /reference/base /kinect2_link"- 
-    At time 1557208373.500
-    - Translation: [0.097, -0.127, 0.890]
-    - Rotation: in Quaternion [-0.017, 0.524, -0.002, 0.852]
-                in RPY (radian) [-0.067, 1.103, -0.045]
-                in RPY (degree) [-3.864, 63.181, -2.591]
-                
-    This function returns the command to run the static transform
+    This function returns the command to run the static transform between the base and kinect frames
     '''
     Y = euler[2]
     P = euler[1]
     R = euler[0]
-    
-    Y_ = (np.pi/2) - R 
-    P_ = np.pi - Y
-    R_ = (np.pi/2) - P
-    
-    print("There you go - ")
-    print("rosrun tf static_transform_publisher {} {} {} {} {} {} /reference/base /kinect2_link 10".format(trans[0], trans[1], trans[2], Y_, P_, R_))
+
+    Y_ = R 
+    P_ = Y - np.pi/2  
+    R_ = P + np.pi/2 
+    print "<node pkg='tf' type='static_transform_publisher' name='kinect_broadcaster' args='{} {} {} {} {} {} {} {} 10' />".format(trans[0], trans[1], trans[2], Y_, P_, R_, base_frame, kinect_frame)
+    return "rosrun tf static_transform_publisher {} {} {} {} {} {} {} {} 10".format(trans[0], trans[1], trans[2], Y_, P_, R_, base_frame, kinect_frame)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--euler", nargs="*", type=float, default=[0,0,0], help="euler transform returned to you by baxter_h2r_kinect_calibration - RPY format in radians")
-    parser.add_argument("--trans", nargs="*", type=float, default=[0,0,0], help="translation vector")
+    parser.add_argument("base_frame", help="frame we are grounding kinect frame in", type=str)
+    parser.add_argument("kinect_frame", help="name of newly created kinect frame", type=str)
     args = parser.parse_args()
-    main(args.trans, args.euler)
+    
+    rospy.init_node("kinect_transform", anonymous=True)
+    listener = tf.TransformListener()
+    try:
+        listener.waitForTransform(args.base_frame, args.kinect_frame, rospy.Time(0), rospy.Duration(5.0))
+        transform = listener.lookupTransform(args.base_frame, args.kinect_frame, rospy.Time(0))
+        trans = transform[0]
+        euler = tf.transformations.euler_from_quaternion(transform[1])
+        output = transform_command(args.base_frame, args.kinect_frame, trans, euler)
+        print output
+
+        response = raw_input("Would you like to run the transform now? ")
+        if response in ["y", "Y", "yes", "Yes"]:
+            rospy.loginfo("Broadcasting transform between {} and {}".format(args.base_frame, args.kinect_frame))
+            os.system(output)
+            rospy.loginfo("Exiting transform broadcast...")
+        else:
+            rospy.loginfo("Broadcast canceled")
+
+    except Exception as e:
+        rospy.logerr(e)
+    
+
+
+
+    
